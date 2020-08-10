9538f34ed139a506870ffffa15de5f2381d9814c
colaxyz <linjunxyz@outlook.com>
2020-08-11 00:51:25 +0800

    ...

--- eladmin-system/src/main/java/me/zhengjie/modules/system/rest/VerifyController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/rest/VerifyController.java
@@ -3,14 +3,16 @@ package me.zhengjie.modules.system.rest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import me.zhengjie.domain.vo.EmailVo;
-import me.zhengjie.service.EmailService;
 import me.zhengjie.modules.system.service.VerifyService;
 import me.zhengjie.utils.enums.CodeBiEnum;
 import me.zhengjie.utils.enums.CodeEnum;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Objects;
 
 /**
@@ -24,15 +26,6 @@ import java.util.Objects;
 public class VerifyController {
 
     private final VerifyService verificationCodeService;
-    private final EmailService emailService;
-
-    @PostMapping(value = "/email/resetPass")
-    @ApiOperation("重置密码，发送验证码")
-    public ResponseEntity<Object> resetPass(@RequestParam String email){
-        EmailVo emailVo = verificationCodeService.sendEmail(email, CodeEnum.EMAIL_RESET_PWD_CODE.getKey());
-        emailService.send(emailVo,emailService.find());
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
 
     @GetMapping(value = "/validated")
     @ApiOperation("验证码验证")
