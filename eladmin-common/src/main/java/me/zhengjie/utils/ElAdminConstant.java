f352fec27ee0dfa2cf8655ba657eb36d00fe1cbe
colaxyz <linjunxyz@outlook.com>
2020-08-18 09:07:12 +0800

    ...

--- eladmin-common/src/main/java/me/zhengjie/utils/ElAdminConstant.java
+++ eladmin-common/src/main/java/me/zhengjie/utils/ElAdminConstant.java
@@ -18,11 +18,4 @@ public class ElAdminConstant {
      */
     public static final String MAC = "mac";
 
-    /**
-     * 常用接口
-     */
-    public static class Url {
-        // IP归属地查询
-        public static final String IP_URL = "http://whois.pconline.com.cn/ipJson.jsp?ip=%s&json=true";
-    }
 }
