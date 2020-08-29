a2c47d568b666e7b3931cefe6dab9da40dfe1175
colaxyz <linjunxyz@outlook.com>
2020-08-29 20:55:53 +0800

    ...

--- eladmin-system/src/main/java/me/zhengjie/modules/security/config/bean/LoginProperties.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/security/config/bean/LoginProperties.java
@@ -6,18 +6,9 @@ import lombok.Data;
 public class LoginProperties {
 
     /**
-     * 账号单用户 登录
-     */
-    private boolean singleLogin = false;
-
-    /**
      * 用户登录信息缓存
      */
     private boolean cacheEnable;
-
-    public boolean isSingleLogin() {
-        return singleLogin;
-    }
 
     public boolean isCacheEnable() {
         return cacheEnable;
