c3b2fd70bec9175763aefd29953120fd01a5fcf9
colaxyz <linjunxyz@outlook.com>
2020-08-30 23:09:48 +0800

    ...

--- eladmin-common/src/main/java/me/zhengjie/utils/CacheKey.java
+++ eladmin-common/src/main/java/me/zhengjie/utils/CacheKey.java
@@ -12,7 +12,6 @@ public interface CacheKey {
      */
     String USER_MODIFY_TIME_KEY = "user:modify:time:key:";
     String APP_MODIFY_TIME_KEY = "app:modify:time:key:";
-    String JOB_MODIFY_TIME_KEY = "job:modify:time:key:";
     String MENU_MODIFY_TIME_KEY = "menu:modify:time:key:";
     String ROLE_MODIFY_TIME_KEY = "role:modify:time:key:";
     String DEPT_MODIFY_TIME_KEY = "dept:modify:time:key:";
--- eladmin-system/src/main/java/me/zhengjie/modules/security/service/UserCacheClean.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/security/service/UserCacheClean.java
@@ -3,19 +3,11 @@ package me.zhengjie.modules.security.service;
 import me.zhengjie.utils.StringUtils;
 import org.springframework.stereotype.Component;
 
-/**
- * @author: liaojinlong
- * @date: 2020/6/11 18:01
- * @apiNote: 用于清理 用户登录信息缓存
- */
 @Component
 public class UserCacheClean {
 
     /**
-     * 清理特定用户缓存信息<br>
-     * 用户信息变更时
-     *
-     * @param userName /
+     * 清理特定用户缓存信息
      */
     public void cleanUserCache(String userName) {
         if (StringUtils.isNotEmpty(userName)) {
