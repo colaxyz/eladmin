f925c1b96f02d559f0f9dce1e27177369164cb10
colaxyz <linjunxyz@outlook.com>
2020-11-10 21:41:09 +0800

    .

--- eladmin-system/src/test/java/me/zhengjie/EladminSystemApplicationTests.java
+++ eladmin-system/src/test/java/me/zhengjie/EladminSystemApplicationTests.java
@@ -1,19 +1,8 @@
 package me.zhengjie;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class EladminSystemApplicationTests {
+@SpringBootTest
+class EladminSystemApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
-
-    public static void main(String[] args) {
-    }
 }
-
